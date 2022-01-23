d4ff275d89bbd4d73c36c84734c53b2b86283694
Dan Payne <dpayne5532@gmail.com>
2022-01-22 18:28:04 -0700

    Initial Commit

---
+++ PDFPlayground/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  PDFPlayground
+//
+//  Created by Dan Payne on 1/20/22.
+//
+
+import SwiftUI
+
+
+
+
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
---
+++ PDFPlayground/PDFPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFPlaygroundApp.swift
+//  PDFPlayground
+//
+//  Created by Dan Payne on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct PDFPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
